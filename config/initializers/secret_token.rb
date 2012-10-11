b7cdfa8675b34789fa5ee30dd8c69196c68ce590
root <root@marahovv-X38-DS4.(none)>
2012-10-11 22:08:32 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '14a32b5380723f10b8cc21322a33ac38ecb2cbe37d15b4ad82fef7d54515635773512d9731d38c30c663b667475c99e0c427ee139d7b574acff2e1cf5e8cc06e'
